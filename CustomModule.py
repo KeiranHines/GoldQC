a4af2e4a9a54dbcd08a1b329bfcec246062bc8db
Keiran Hines <keiranhines@gmail.com>
2017-08-15 15:33:11 +1000

    Started working on extracting input information from GoldSim

--- CustomModule.py
+++ CustomModule.py
@@ -211,7 +211,7 @@ either expressed or implied, of the FreeBSD Project.
 """
 #===========================================================================
 # imports
-import TestModule as CImp
+import GoldQC as CImp
 #BAD_NAME = "
 #===========================================================================
 # MODULE GLOBAL parameters/values.
@@ -271,10 +271,10 @@ GS_TIMESERIES_TYPE_IND = 2  # index in array where time series type goes
 #           NL[2] = the description for this item
 #           NL[3] = the time series type; only for time series
 
-""" GOLDSIM PROVIDED
+"""
 IN_VAR_LIST = [ [ 1, DBL_TYPER, "Test 1" ] ]
 """
-""" GOLDSIM PROVIDED
+"""
 IN_VAR_LIST = [ [ 1, DBL_TYPER, "Start Time (date time)" ],
                 [ [3,4], MATRIX_TYPER, "Matrix" ],
                 [ 4, VECTOR_TYPER, "Vector" ],
@@ -283,8 +283,7 @@ IN_VAR_LIST = [ [ 1, DBL_TYPER, "Start Time (date time)" ],
                 [ 12, TS_TYPER, "Elapsed Time Series", ELAPSED_TIME ],
                 [ 1, DBL_TYPER, "Time Series Multiplier" ] ]
 """
-# Mine for testing GoldQC
-IN_VAR_LIST = [[VECTOR_TYPER, "Input_Vector"]]
+IN_VAR_LIST = [ [7,VECTOR_TYPER,"inputVector" ] ]
 
 #---------------------------------------------------------------------------
 # RET_VAR_LIST provides the list of return variables to GoldSim. This must
@@ -310,11 +309,10 @@ RET_VAR_LIST = [ [ 3, LUTABLE_1D_TYPER, "1D Lookup Table" ],
                  [ 12, TS_TYPER, "Calendar Time Series", CALENDAR_TIME ],
                  [ [4,3], LUTABLE_2D_TYPER, "2D Lookup Table" ],
                  [ 12, VECTOR_TYPER, "Vector" ],
-                 [ 1, DBL_TYPER, "Number of Inputs" ] ]
+                  [ 1, DBL_TYPER, "Number of Inputs" ] ]
 """
-# Mine for Testing GoldQC
-RET_VAR_LIST = [[ 1, [VECTOR_TYPER, "Output_Vector" ]]
 
+RET_VAR_LIST = [ [7,VECTOR_TYPER,"outputVector" ] ]
 #---------------------------------------------------------------------------
 # set the debug level. 0 = no output; 1 = will output to file input argument
 # list and output return list.
--- GoldQC.py
+++ GoldQC.py
@@ -1,147 +1,162 @@
+# -*- coding: utf-8 -*-
 """
-Copyright 2017, Keiran Hines
-This file is part of GoldQC.
+Python Module: TestModule.py
+Created by: Nick Martin, GoldSim Technology Group
+Creation Date: 23 May 2015
+Last Edited: 25 May 2015
 
-GoldQC is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+License: FreeBSD License (reproduced below)
 
-GoldQC is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+Purpose:
 
-You should have received a copy of the GNU General Public License
-along with GoldQC.  If not, see <http://www.gnu.org/licenses/>.
+Provides a rudimentary implementation of the CustomPython.dll linkage for
+the GoldSim external element. Primarily confirms that pass the various
+supported element type values in the right format.
+
+The "required" functions to implement are:
+
+SetStuffUp
+WrapUpLogFile
+MyCustomCalculations
+
+The "required" parameters are:
+
+CUSTOM_MODULE_VERSION
+LOG_FILE_NAME
+
 """
-import os, sys
-import pythoncom,pywintypes
-from win32com.client import Dispatch
-from Exceptions import DatabaseNotConnectedException
-import datetime
 
+"""
+LICENSE
+
+This module is available for your use under the FreeBSD License, see:
+http://directory.fsf.org/wiki?title=License:FreeBSD 
+
+FreeBSD License
+
+Copyright (c) 2015, GoldSim Technology Group LLC
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies,
+either expressed or implied, of the FreeBSD Project.
+"""
+
+#===========================================================================
 # Module level globals.
 CUSTOM_MODULE_VERSION = 0.1         # module version --- REQUIRED
 LOG_FILE_NAME = "TestLog.txt"       # log file name --- REQUIRED
 PHREEQC = None
+STEP = 0
+#BAD_NAME = "
+#===========================================================================
+# functions
 
-
-def SetStuffUp():
+def SetStuffUp( ):
     """
-    Required function for Linkage.py to link the GoldSim technologies package an IPHREEQC COM connection
-    :return:
-        Integer: 0 = good; 1 = something went wrong
+    Required function from CustomModule.py. Does whatever is needed in terms
+    of set-up. Here just writes the initial entry to the log file.
+    
+    Return:
+    
+        Integer status: 0 = good; 1 = bad
+    
     """
-    global PHREEQC
+    # globals
     global LOG_FILE_NAME
-    db_path = "database/phreeqc.dat"
+    global PHREEQC
+
+    # local imports
+    import datetime
     with open(LOG_FILE_NAME, 'w', 0) as Log:
-        try:
-            PHREEQC = Dispatch('IPhreeqcCOM.Object')
-        except pythoncom.ole_error:
-            Log.write("Error Could not find IPhreeqcCOM, are you sure its installed?\n")
-            return 1
-        try:
-            PHREEQC.LoadDatabase(db_path) # TODO Make more generic maybe with a config file.
-        except pywintypes.com_error:
-            Log.write("Error Could not load database file %s\n" % db_path)
-            return 1
         Log.write("Starting GoldQC.py script at %s.\n\n" % datetime.datetime.now().strftime("%x %H:%M"))
+    import pythoncom, pywintypes
+    from win32com.client import Dispatch
+
+    # start of function.
+    db_path = "database/phreeqc.dat"
+    try:
+        PHREEQC = Dispatch('IPhreeqcCOM.Object')
+    except pythoncom.ole_error:
+        with open(LOG_FILE_NAME, 'a', 0) as Log:
+            Log.write("Error Could not find IPhreeqcCOM, are you sure its installed?\n")
+        return 1
+    try:
+        PHREEQC.LoadDatabase(db_path) # TODO Make more generic maybe with a config file.
+    except pywintypes.com_error:
+        with open(LOG_FILE_NAME, 'a', 0) as Log:
+            Log.write("Error Could not load database file %s\n" % db_path)
+        return 1
+    with open(LOG_FILE_NAME, 'a', 0) as Log:
+        Log.write("Successfully Started GoldQC.py script at %s.\n\n" % datetime.datetime.now().strftime("%x %H:%M"))
     return 0
 
-def WrapUpLogFile(logfilename):
+def WrapUpLogFile( LogFileName ):
     """
     Required to end off the log file
     :param logfilename: the name of the logfile
     :return: None
     """
-
+    #globals
     global LOG_FILE_NAME
+
+    #local imports
+    import datetime
     with open(LOG_FILE_NAME, 'a', 0) as Log:
         Log.write("GoldQC.py completed successfully at %s.\n\n" % datetime.datetime.now().strftime("%x %H:%M"))
     return
 
-def MyCustomCalculations(PyInputList):
+def MyCustomCalculations(input):
     """
-    Required function for completing the PHREEQC side of the calculations first processes the input from GoldSim into
-    a PHREEQC input/SelectedOutput string then calls process_input to have it processed by PHREEQC
-    :param PyInputList: a list of values provided from GoldSim for processing
-    :return: RET_VAR_LIST list of output values in the form GoldSim Expects them to be returned.
+    Required wrapper function for either completing the custom calculations 
+    or for calling other functions and modules to complete these calculations.
+    
+    Arguments:
+    
+        PyInputList = a list of the input values/parameters which has come
+                        from the GoldSim external element via CustomModule.py.
+    
+    Return:
+    
+        ReturnList = list of the output values which needs to be in the format
+                        expected by CustomModule.py.
+    
+    RET_VAR_LIST = [ [ 3, LUTABLE_1D_TYPER, "1D Lookup Table" ],
+                 [ [4,4], MATRIX_TYPER, "Matrix" ],
+                 [ 12, TS_TYPER, "Elapsed Time Series", ELAPSED_TIME ],
+                 [ 1, DBL_TYPER, "Number of Inputs" ],
+                 [ 12, TS_TYPER, "Calendar Time Series", CALENDAR_TIME ],
+                 [ [4,3], LUTABLE_2D_TYPER, "2D Lookup Table" ],
+                 [ 12, VECTOR_TYPER, "Vector" ],
+                 [ 1, DBL_TYPER, "Number of Inputs" ] ]
+    
     """
-    #imports
+    #globals
+    global LOG_FILE_NAME
+    global STEP
+    STEP = STEP + 1
+    # imports
     from CustomModule import RET_VAR_LIST
-
-
-
-def process_input(input_string):
-    """
-    Runs a selected input string on the PHREEQC connection and returns the output
-    :param input_string: input for simulation
-    :return: @see Dispatch.getSelectedOutputArray()
-    """
-
-    if not PHREEQC:
-        raise DatabaseNotConnectedException("PHREEQC database not connected")
-        return
-    PHREEQC.RunString(input_string)
-    output = PHREEQC.GetSelectedOutputArray()
-    with open(LOG_FILE_NAME, 'a', 0) as Log:
-        Log.write("Output-----------------\n\n")
-        Log.write(output)
-        Log.write("End-of-Output----------\n\n")
-    return output
-
-class IPhreeqcConnection:
-
-    def __init__(self):
-        self.dbase = None
-
-    def connect_database(self, db_path):
-        """
-        Loads the PHREEQC COM connection and connects PHREEQC to the requested database file.
-        :param db_path:
-        """
-        try:
-            self.dbase = Dispatch('IPhreeqcCOM.Object')
-        except pythoncom.ole_error:
-            sys.stderr.write("Error Could not find IPhreeqcCOM, are you sure its installed?\n")
-            exit(1)
-        try:
-            self.dbase.LoadDatabase(db_path)
-        except pywintypes.com_error:
-            sys.stderr.write("Error Could not load database file %s\n"%(db_path))
-            exit(1)
-
-    @staticmethod
-    def make_selected_output(components):
-        """
-       Build SELECTED_OUTPUT data block to retrieve only important information from the PHREEQC calculation
-       :param components The list of components from the database input. @see IPHreeqc.COM GetComponentList()
-
-       """
-        headings = "-headings    cb    H    O    "
-        headings += '\t'.join(components)
-        selected_output = """
-               SELECTED_OUTPUT
-                   -reset false
-               USER_PUNCH
-               """
-        selected_output += headings + "\n"
-        # charge balance, H, and O
-        code = '10 w = TOT("water")\n'
-        code += '20 PUNCH CHARGE_BALANCE, TOTMOLE("H"), TOTMOLE("O")\n'
-        # All other elements
-        lino = 30
-        for component in components:
-            code += '%d PUNCH w*TOT(\"%s\")\n' % (lino, component)
-            lino += 10
-        selected_output += code
-        return selected_output
-
-
-if __name__ == '__main__':
-    # TESTING ONLY TODO REMOVE
     INPUT_STRING = """
         SOLUTION 1
         END
@@ -158,18 +173,91 @@ if __name__ == '__main__':
         \t-step true
         \t-high_precision true
         END"""
-    iphreeqc = IPhreeqcConnection()
-    try:
-        iphreeqc.connect_database("database/phreeqc.dat")
-    except OSError as e:
-        print e
-    results = iphreeqc.process_input(INPUT_STRING) #Update to read from IPHREEQC database
-    heading = ''
-    for r in results[0]:
-        heading += r + "\t"
-    print heading
-    for r in results[1:]:
-        line = ''
-        for i in r:
-            line += str(i) + "\t"
-        print line
+    ReturnList = list()
+    listLen = len(input)
+    vector = input[0]
+    with open(LOG_FILE_NAME, 'a',0) as Log:
+        Log.write("Input on Step %d\n" %STEP)
+    with open(LOG_FILE_NAME, 'a',0) as Log:
+        for i in vector:
+            Log.write(str(i) + '\n')
+        return ReturnList
+
+
+def process_input(input_string):
+    """
+    Runs a selected input string on the PHREEQC connection and returns the output
+    :param input_string: input for simulation
+    :return: @see Dispatch.getSelectedOutputArray()
+    """
+
+    #globals
+    global LOG_FILE_NAME
+
+    #local imports
+    from Exceptions import DatabaseNotConnectedException
+    if not PHREEQC:
+        raise DatabaseNotConnectedException("PHREEQC database not connected")
+        return
+    PHREEQC.RunString(input_string)
+    output = PHREEQC.GetSelectedOutputArray()
+    with open(LOG_FILE_NAME, 'a', 0) as Log:
+        Log.write("Output-----------------\n\n")
+        for o in output:
+            Log.write(str(o))
+            Log.write("\n")
+        Log.write("End-of-Output----------\n\n")
+    return output
+
+
+# Block to run as standalone script. This can be run from the command line
+# using python TestModule.py. This standalone script block provides a good
+# way to test your custom implementation. This testing is important because
+# need to implement the Python interpreter functions imported from 
+# CustomModule without error in order to have the dll work correctly. In
+# other words, errors thrown from the Python interpreter will not make it
+# back to GoldSim.
+# This example standalone block assumes that IN_VAR_LIST has been changed to
+# one double type inputs and that RET_VAR_LIST has been changed to one double 
+# type output. Need to have MyCustomCalculations return a list which has 
+# one item which is a list containing a float value.
+if __name__ == "__main__":
+    # local imports
+    SetStuffUp()
+    t = list()
+    MyCustomCalculations(t)
+    import sys
+    from CustomModule import InitialChecks, PyModuleVersion, CalcInputs
+    from CustomModule import CalcOutputs, CustomCalculations, WrapUpStuff
+    # local variables
+    RetStatus1 = 0              # integer return status
+    ErrorMsg = ""               # error message.
+    ModelVer = 0.0              # the model versions
+    NumInputs = 0               # the number of inputs
+    NumOutputs = 0              # the number of outputs
+    RetList = list()            # the return list
+    InList = list()             # testing input list.
+    # now do the calculaitons.
+    RetStatus1 = InitialChecks()
+    if RetStatus1 != 0:
+        # this is an error
+        ErrorMsg = "Found return status of %d from InitialChecks(). This " \
+                    "denotes an error.\n" % RetStatus1
+        sys.exit( ErrorMsg )
+    # get the model versions
+    ModelVer = PyModuleVersion()
+    print( "The module version %g.\n" % ModelVer )
+    # get the number of inputs and outputs
+    NumInputs = CalcInputs()
+    NumOutputs = CalcOutputs()
+    print( "Number of inputs expected is %d \n" % NumInputs )
+    print( "Number of outputs expected is %d \n" % NumOutputs )
+    # now do the main part.
+    InList.append( 22.5 )
+    RetList = CustomCalculations( InList, NumOutputs )
+    print( "Return list is %s " % RetList )
+    # now do the wrap up.
+    WrapUpStuff()
+    # now are done
+    
+# EOF
