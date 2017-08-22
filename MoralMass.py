558bdabc14fd4d8e042eecaa158cd4beafdfe4d5
Keiran Hines <keiranhines@gmail.com>
2017-08-22 12:51:16 +1000

    Added MolarMass for conversion from mol/kg to mg/l

---
+++ MoralMass.py
@@ -0,0 +1,44 @@
+"""
+@Autor Keiran Hines, RMIT
+
+Purpose helper functions and data for handling data transfer between GoldSim and Iphreeqc
+"""
+
+"""
+Molar Mass values provided by Lenntech, available at http://www.lenntech.com/periodic/mass/atomic-mass.htm
+"""
+MOLAR_MASS_LIST = {
+    "Al": 26.9815,
+    "As": 74.9216,
+    "B": 10.811,
+    "Ba": 137.327,
+    "Be": 9.0122,
+    "Br": 79.904,
+    "Ca": 40.078,
+    "Cd": 112.411,
+    "Cl": 35.453,
+    "Co": 58.9332,
+    "Cr": 51.9961,
+    "Cu": 63.546,
+    "F": 18.9984,
+    "Fe": 55.845,
+    "K": 39.0983,
+    "Mg": 24.305,
+    "Mn": 54.938,
+    "Mo": 95.94,
+    "Ni": 58.6934,
+    "Na": 22.9897,
+    "P": 30.9738,
+    "Pb": 207.2,
+    "S(6)": 96.06,
+    "Sb": 121.76,
+    "Se": 78.96,
+    "Si": 28.0855,
+    "Sn": 118.71,
+    "Sr": 87.62,
+    "U": 238.0289,
+    "V": 50.9415,
+    "Zn": 65.39,
+    "Ag": 107.8682,
+    "Hg": 200.59
+}
